554fac70a50d5f1a698d670f332df5331fa27954
Whuss88 <williamhussey95@gmail.com>
2024-10-09 13:52:30 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//
+
+//equation to equal 10 5+5=10
+let code1 = 5;
+code1 = code1 + 5;
+console.log(code1);
+//equation to equal 40 45-5=40
+let code2 = 45;
+code2 = code2 - 5;
+console.log(code2)
+//equation to equal 39 39*1=39
+let code3 = 39;
+code3 = code3 * 1;
+console.log(code3);
+
+const combination = `Vault code is ${code1}, ${code2}, ${code3}`
+console.log(combination)
+
+//create a popup dialog box displaying codes
+alert (`You have received this message because you have been chosen to open an important vault. Here is the secret combination:10-40-39`)
---
+++ index2.js
@@ -0,0 +1,2 @@
+// create alert saying congrats vault is now open
+alert (`Vault is now open, enjoy!`)
